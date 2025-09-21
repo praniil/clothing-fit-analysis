2b9e00a4da30f95c8b78e46b6aaa444fba44013e
praniil <pparajuli15@gmail.com>
2025-09-21 19:41:32 +0545

    feat: face detection along with eye detection

--- src/facedetection.py
+++ src/facedetection.py
@@ -2,7 +2,7 @@ from PIL import Image
 import numpy as np
 import cv2
 
-image_path = '../dataset/raw_dataset/skin_tone_testset/gyale.jpeg'
+image_path = '../dataset/raw_dataset/skin_tone_testset/whiteblack.jpg'
 img = Image.open(image_path)
 
 image_array = np.array(img)
@@ -21,15 +21,19 @@ print(gray_image)
 faces = face_cascade.detectMultiScale(gray_image, 1.1, 4, minSize=(40, 40))
 
 for (x, y, w, h) in faces:
-    cv2.rectangle(image_array, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    #for eye detection
+    eye_gray_roi = gray_image[y:y+h, x:x+w]
+    eye_image_roi = image_array[y:y+h, x:x+w]
 
-#for eye detection
-eye_gray_roi = gray_image[y:y+h, x:x+w]
-eye_image_roi = image_array[y:y+h, x:x+w]
+    eyes = eye_cascade.detectMultiScale(eye_gray_roi)
+    print(f"eyes: {eyes}")
+    if len(eyes) != 0:
+        cv2.rectangle(image_array, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-eyes = eye_cascade.detectMultiScale(eye_gray_roi)
-for(ex, ey, ew, eh) in eyes:
-    cv2.rectangle(eye_image_roi, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+    for(ex, ey, ew, eh) in eyes:
+        cv2.rectangle(eye_image_roi, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
 
 cv2.imshow('detected faces', image_array)
 cv2.waitKey(0)
