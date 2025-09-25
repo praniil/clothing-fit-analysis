67d0747c780106dd8445d0bffa2de50d94af95e8
praniil <pparajuli15@gmail.com>
2025-09-25 12:03:52 +0545

    feat: computed the final result of skin tone classifier by using the highest number of predicted skin tone among all the predicted skin tone color

--- src/facedetection/facedetection_realtime_opencv.py
+++ src/facedetection/facedetection_realtime_opencv.py
@@ -1,22 +1,29 @@
 import cv2
 import time
 import os, sys
+import numpy as np
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) # Python can now find modules in any subfolder of src.
-
 from skin_tone_classifier.test_finetuned_resnet18 import predict_skin_tone
 
-print(__file__)
 
+#capture the video frames
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
     raise IOError("Cannot open webcam")
 
+# load the opencv models
 face_cascade = cv2.CascadeClassifier('../../open_cv_models/haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('../../open_cv_models/haarcascade_eye.xml')
+# eye_cascade = cv2.CascadeClassifier('../../open_cv_models/haarcascade_eye.xml')
 
+# global count variables
 screenshot_count = 0;
+total_screenshot_need = 15;
+brown = 0
+white = 0
+black = 0
 
+# maintain the opencv_screenshot folder -- delete the previous screenshots
 folder_name = "../../opencv_screenshots"
 os.makedirs(folder_name, exist_ok=True)
 
@@ -30,6 +37,7 @@ for item in dir:
     
 print(f"Content of {folder_name} cleared")
 
+# video capture loop
 while 1:
     ret, frame = cap.read()
 
@@ -49,22 +57,41 @@ while 1:
         # Detects eyes of different sizes in the input image
         # eyes = eye_cascade.detectMultiScale(roi_gray, minSize=(5, 5)) 
 
-        # #To draw a rectangle in eyes
+        #To draw a rectangle in eyes
         # for (ex,ey,ew,eh) in eyes:
-        #     cv2.rectangle(face_crop,(ex,ey),(ex+ew,ey+eh),(0,127,255),2)
+            # cv2.rectangle(face_crop,(ex,ey),(ex+ew,ey+eh),(0,127,255),2)
         
     # Display an image in a window
     cv2.imshow('img', frame)
 
     current_timestamp_time = time.time() * 1000
     
-    if screenshot_count < 15 and len(faces) > 0:
+    if screenshot_count < total_screenshot_need and len(faces) > 0:
         print(current_timestamp_time)
         cv2.imwrite(f"{folder_name}/{current_timestamp_time}.jpg", face_crop)
-        screenshot_count+=1
         image_path_array.append(f"{folder_name}/{current_timestamp_time}.jpg")
         result = predict_skin_tone(image_path_array[-1])
-        print(f"Predicted Skin Tone for {image_path_array[-1]} is: {result}")
+
+        if result == 'Brown':
+            brown += 1
+        elif result == 'White':
+            white += 1
+        else:
+            black += 1
+
+        if screenshot_count == total_screenshot_need - 1:
+            if brown > white and brown > black:
+                end_result = "Brown"
+            elif white > brown and white > black:
+                end_result = "Black"
+            else:
+                end_result = "White"
+
+            print(f"Brown: {brown}, Black: {black}, White: {white}")
+
+            print(f"Predicted Skin Tone: {end_result}")
+
+        screenshot_count+=1
 
     # Wait for Esc key to stop
     k = cv2.waitKey(30) & 0xff
